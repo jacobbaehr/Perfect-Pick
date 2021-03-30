3489ef82b63b937f3c2c2307f6472a67ef2db4d5
Mattheww31 <49414872+Mattheww31@users.noreply.github.com>
2021-03-30 17:24:15 -0400

    Database code added Database Code

--- app/src/main/java/com/example/algorithmsanonymous/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/algorithmsanonymous/ui/login/LoginActivity.kt
@@ -1,10 +1,12 @@
 package com.example.algorithmsanonymous.ui.login
 
+
+import android.R.attr
 import android.app.Activity
-import android.app.PendingIntent.getActivity
 import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
+import android.text.TextUtils
 import android.text.TextWatcher
 import android.view.View
 import android.view.inputmethod.EditorInfo
@@ -18,22 +20,67 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.algorithmsanonymous.MainActivity
 import com.example.algorithmsanonymous.R
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.IgnoreExtraProperties
+import kotlinx.android.synthetic.main.activity_login.*
 
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity: AppCompatActivity() {
+
+
+
+    lateinit var inputEmail: EditText
+    lateinit var inputPassword: EditText
+    lateinit var btnSignIn: Button
+    lateinit var btnSignUp: Button
+    lateinit var btnResetPassword: Button
+    lateinit var progressBar: ProgressBar
+    lateinit var auth: FirebaseAuth
+
+
+    var database: FirebaseDatabase? = FirebaseDatabase.getInstance()
+
+
 
     private lateinit var loginViewModel: LoginViewModel
+    private var mAuth: FirebaseAuth? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    // Write a message to the database
+
+
+
+     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
         setContentView(R.layout.activity_login)
+
+
+        auth = FirebaseAuth.getInstance()
+
+
+
         //setContentView(R.layout.activity_main)
+
+        btnSignIn = findViewById<Button>(R.id.login)
+        btnSignUp = findViewById<Button>(R.id.login)
+        inputEmail = findViewById<EditText>(R.id.username)
+        inputPassword = findViewById<EditText>(R.id.username)
+
+
+
 
         val username = findViewById<EditText>(R.id.username)
         val password = findViewById<EditText>(R.id.password)
         val login = findViewById<Button>(R.id.login)
         val loading = findViewById<ProgressBar>(R.id.loading)
+
+
 
         loginViewModel = ViewModelProvider(this, LoginViewModelFactory())
                 .get(LoginViewModel::class.java)
@@ -96,9 +143,86 @@ class LoginActivity : AppCompatActivity() {
             }
 
             login.setOnClickListener {
+
                 loading.visibility = View.VISIBLE
                 loginViewModel.login(username.text.toString(), password.text.toString())
+
+                val email1 = inputEmail.text.toString().trim { it <= ' ' }
+                val password = inputPassword.text.toString().trim { it <= ' ' }
+
+                if (TextUtils.isEmpty(email1)) {
+                    Toast.makeText(
+                        applicationContext,
+                        "Enter email address!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(applicationContext, "Enter password!", Toast.LENGTH_SHORT)
+                        .show()
+
+                }
+
+                if (password.length < 6) {
+                    Toast.makeText(
+                        applicationContext,
+                        "Password too short, enter minimum 6 characters!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                }
+
+                auth.createUserWithEmailAndPassword(email1, password)
+                    .addOnCompleteListener(
+
+                        this@LoginActivity,
+                        OnCompleteListener<AuthResult?> { task ->
+                            Toast.makeText(
+                                this@LoginActivity,
+                                "createUserWithEmail:onComplete:" + task.isSuccessful,
+                                Toast.LENGTH_SHORT
+                            ).show()
+
+                            // If sign in fails, display a message to the user. If sign in succeeds
+                            // the auth state listener will be notified and logic to handle the
+                            // signed in user can be handled in the listener.
+                            if (!task.isSuccessful) {
+                                Toast.makeText(
+                                    this@LoginActivity,
+                                    "Authentication passed." + task.isSuccessful,
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                                auth.signInWithEmailAndPassword(email1, password)
+                                        .addOnCompleteListener(this@LoginActivity) { task ->
+                                            // If sign in fails, display a message to the user. If sign in succeeds
+                                            // the auth state listener will be notified and logic to handle the
+                                            // signed in user can be handled in the listener.
+                                            progressBar.visibility = View.GONE
+                                            if (!task.isSuccessful) {
+                                                // there was an error
+                                                if (password.length < 6) {
+
+                                                } else {
+//                                                    Toast.makeText(this@LoginActivity, getString(R.string.auth_failed), Toast.LENGTH_LONG).show()
+                                                }
+                                            } else {
+                                                val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                                                startActivity(intent)
+                                                finish()
+                                            }
+                                        }
+                            } else {
+
+
+                            }
+
+                        })
+                writeNewUser("1", username.toString(), email1)
+
             }
+
         }
     }
 
@@ -115,13 +239,111 @@ class LoginActivity : AppCompatActivity() {
         // CHRIS-LINDSEY: This is where we switch from the login page to the main activity file/page
         startActivity (
                 Intent(this,
-                MainActivity::class.java
-        ))
+                        MainActivity::class.java
+                ))
+    }
+    private fun updateUiWithUser(model: FirebaseUser?) {
+        val welcome = getString(R.string.welcome)
+        val displayName = model?.displayName
+
+        Toast.makeText(
+                applicationContext,
+                "$welcome $displayName",
+                Toast.LENGTH_LONG
+        ).show()
+
+        // CHRIS-LINDSEY: This is where we switch from the login page to the main activity file/page
+        startActivity (
+                Intent(this,
+                        MainActivity::class.java
+                ))
     }
 
+
+    private fun createAccount(email: String, password: String, name: String, mobile: String) {
+
+        mAuth!!.createUserWithEmailAndPassword(email, attr.password.toString())
+                .addOnCompleteListener(this) { task ->
+
+
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+
+                        val user = mAuth!!.currentUser
+                        updateUiWithUser(user)
+                    }
+
+                    // ...
+                }
+
+    }
+
+    fun writeNewUser(userId: String, name: String, email: String) {
+
+        var myRef1: DatabaseReference = database!!.getReference("message")
+        var myRef2: DatabaseReference = database!!.getReference("message")
+        var myRef3: DatabaseReference = database!!.getReference("message")
+        var myRef4: DatabaseReference = database!!.getReference("message")
+        var myRef5: DatabaseReference = database!!.getReference("message")
+        var post1 = myRef1.child("friends").child("email").child(userId).setValue(email)
+        var post2 = myRef2.child("friends").child("friend2").child(userId).setValue("Jimo")
+        var post3 = myRef3.child("friends").child("friend3").child(userId).setValue("Jim")
+        var post4 = myRef4.child("friends").child("friend4").child(userId).setValue("Jimtron")
+        var post5 = myRef5.child("friends").child("friend1").child(userId).setValue("Jimtron7")
+    }
+    fun writeNewUserWithTaskListeners(userId: String, name: String, email: String, myRef1:DatabaseReference, myRef2:DatabaseReference, myRef3:DatabaseReference, myRef4:DatabaseReference) {
+        val user = User(name, email)
+
+        // [START rtdb_write_new_user_task]
+        myRef1.child("users").child(userId).setValue(user)
+            .addOnSuccessListener {
+                // Write was successful!
+                // ...
+            }
+            .addOnFailureListener {
+                // Write failed
+                // ...
+            }
+        // [END rtdb_write_new_user_task]
+        // [START rtdb_write_new_user_task]
+        myRef2.child("users").child(userId).setValue(user)
+            .addOnSuccessListener {
+                // Write was successful!
+                // ...
+            }
+            .addOnFailureListener {
+                // Write failed
+                // ...
+            }
+        // [END rtdb_write_new_user_task]
+        // [START rtdb_write_new_user_task]
+        myRef3.child("users").child(userId).setValue(user)
+            .addOnSuccessListener {
+                // Write was successful!
+                // ...
+            }
+            .addOnFailureListener {
+                // Write failed
+                // ...
+            }
+        // [END rtdb_write_new_user_task]
+        // [START rtdb_write_new_user_task]
+        myRef4.child("users").child(userId).setValue(user)
+            .addOnSuccessListener {
+                // Write was successful!
+                // ...
+            }
+            .addOnFailureListener {
+                // Write failed
+                // ...
+            }
+        // [END rtdb_write_new_user_task]
+
+    }
     private fun showLoginFailed(@StringRes errorString: Int) {
         Toast.makeText(applicationContext, errorString, Toast.LENGTH_SHORT).show()
     }
+
 }
 
 /**
@@ -137,4 +359,9 @@ fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
 
         override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
     })
+}
+@IgnoreExtraProperties
+data class User(val username: String? = null, val email: String? = null) {
+    // Null default values create a no-argument default constructor, which is needed
+    // for deserialization from a DataSnapshot.
 }
