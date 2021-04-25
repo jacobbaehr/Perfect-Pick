caf50bf12fd794ed652815a3b1bcf33bc2736ad8
Haynes <christopherhaynes33@gmail.com>
2021-04-25 00:45:27 -0400

    Updates to profile frag and nav bar in  main2

--- app/src/main/java/com/example/algorithmsanonymous/MainActivity2.kt
+++ app/src/main/java/com/example/algorithmsanonymous/MainActivity2.kt
@@ -2,11 +2,13 @@ package com.example.algorithmsanonymous
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Parcelable
 import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
@@ -14,6 +16,7 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+
 
 // Vals needed for API implementation
 private const val TAG = "MainActivity"
@@ -28,6 +31,24 @@ class MainActivity2 : AppCompatActivity(), PlacesAdapter.OnItemClickListener{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        // Create val for each fragment on nav bar
+        val searchFragment = SearchFragment()
+        val favoritesFragment = FavoritesFragment()
+        val profileFragment = ProfileFragment()
+
+        // ---------------------
+        // Bottom navigation bar
+        // ---------------------
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            // When user clicks on nav bar, initiate transaction to appropriate fragment
+            when (it.itemId) {
+                R.id.nav_search -> makeCurrentFragment(searchFragment)
+                R.id.nav_favorites -> makeCurrentFragment(favoritesFragment)
+                R.id.nav_profile -> makeCurrentFragment(profileFragment)
+            }
+            true
+        }
 
         val intent = intent
         val term = intent.getStringExtra("INPUT_1")
@@ -105,11 +126,18 @@ class MainActivity2 : AppCompatActivity(), PlacesAdapter.OnItemClickListener{
 
     }
 
-    // PURPOSE: Creation of fragments
-//    private fun makeCurrentFragment(fragment: Fragment) =
-//            supportFragmentManager.beginTransaction().apply {
-//                replace(R.id.fl_wrapper, fragment)
-//                commit()
-//            }
+
+    // Method to move between fragments via FragmentManager
+    private fun makeCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            val fl = findViewById<View>(R.id.fl_wrapper) as FrameLayout
+            fl.removeAllViews()
+            val transaction1: FragmentTransaction = supportFragmentManager.beginTransaction()
+            transaction1.add(R.id.fl_wrapper, fragment)
+            transaction1.commit()
+        }
+
+
+
 
 }
